3bfab4da26e1d6a85a5da3ee72ed0d79f676c6b2
andreimihailescu <andrei.mihailescu994@gmail.com>
2017-09-28 21:17:38 +0300

    Add Car View

---
+++ CarStore/Controllers/CarController.cs
@@ -0,0 +1,27 @@
+﻿using CarStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarStore.Controllers
+{
+    public class CarController : Controller
+    {
+        // GET: Car
+        public ActionResult Index()
+        {
+            var car = new Car()
+            {
+                Id = 1,
+                Model = "M3",
+                Brand = "BMW",
+                SerialNumber = "xc45612"
+
+            };
+
+            return View(car);
+        }
+    }
+}
