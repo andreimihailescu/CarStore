3bd968b0f9d63b6d5c54e6a13a074cf4364f0bf4
andreimihailescu <andrei.mihailescu994@gmail.com>
2017-09-29 23:09:43 +0300

    Design changes

--- CarStore/App_Start/BundleConfig.cs
+++ CarStore/App_Start/BundleConfig.cs
@@ -20,11 +20,13 @@ namespace CarStore
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+                      //"~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap-solar.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      //"~/Content/bootstrap.css",
+                      "~/Content/bootstrap-solar.css",
                       "~/Content/site.css"));
         }
     }
