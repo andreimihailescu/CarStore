fffb5adc7186a2e490ba5cbce4800d6c6950c2a4
andreimihailescu <andrei.mihailescu994@gmail.com>
2017-10-01 16:26:48 +0300

    Add web apis

---
+++ CarStore/App_Start/WebApiConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace CarStore
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            //// Force response as JSON
+            config.Formatters.JsonFormatter.SupportedMediaTypes
+                .Add(new MediaTypeHeaderValue("text/html"));
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ CarStore/Controllers/Api/CarsController.cs
@@ -0,0 +1,54 @@
+﻿using CarStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CarStore.Controllers.Api
+{
+    public class CarsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public CarsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: /api/cars
+        public IHttpActionResult GetCars()
+        {
+            return Ok(_context.Cars.ToList());
+        }
+
+        // GET: /api/cars/id
+        public IHttpActionResult GetCar(int id)
+        {
+            var car = _context.Cars.SingleOrDefault(c => c.Id == id);
+
+            if(car == null)
+            {
+                return NotFound();
+                //return Redirect("http://localhost:53190/api/cats");
+            }
+
+            return Ok(car);
+        }
+
+        // GET: /api/cars?brand=BMW&model=X6
+        public IHttpActionResult GetCar(string model, string brand)
+        {
+            var car = _context.Cars.SingleOrDefault(c => c.Model == model && c.Brand == brand);
+
+            if (car == null)
+            {
+                return NotFound();
+                //return Redirect("http://localhost:53190/api/cats");
+            }
+
+            return Ok(car);
+        }
+    }
+}
